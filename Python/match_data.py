0a9120c2c8d5c321ba7712f65286688967fc2bf2
attkap <130363145+attkap@users.noreply.github.com>
2023-09-29 17:38:39 +0200

    Update match_data.py

--- Python/match_data.py
+++ Python/match_data.py
@@ -12,13 +12,12 @@ install('xlrd')
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
 
 chrome_options = Options()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
-driver = webdriver.Chrome(executable_path=ChromeDriverManager().install(), options=chrome_options)
+driver = webdriver.Chrome("/usr/bin/chromedriver", options=chrome_options)
 
 from bs4 import BeautifulSoup
 import time
