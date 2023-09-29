5532a960518a4b4b0f9bcd99ce1a745fb1ca6219
attkap <130363145+attkap@users.noreply.github.com>
2023-09-29 17:48:33 +0200

    Update match_data.py

--- Python/match_data.py
+++ Python/match_data.py
@@ -11,15 +11,11 @@ install('openpyxl')
 install('xlrd')
 
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.options import Options
 
-chrome_options = Options()
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("--disable-dev-shm-usage")
-
-driver = webdriver.Chrome(executable_path="/usr/bin/chromedriver", options=chrome_options)
-
+firefox_options = Options()
+firefox_options.add_argument("--headless")
+driver = webdriver.Firefox(options=firefox_options)
 
 from bs4 import BeautifulSoup
 import time
