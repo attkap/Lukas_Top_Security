889242ae0a2b5f1701bfe7489a2ebcb6439f48ee
attkap <130363145+attkap@users.noreply.github.com>
2023-09-29 17:42:22 +0200

    Update match_data.py

--- Python/match_data.py
+++ Python/match_data.py
@@ -17,7 +17,9 @@ chrome_options = Options()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
-driver = webdriver.Chrome("/usr/bin/chromedriver", options=chrome_options)
+
+driver = webdriver.Chrome(executable_path="/usr/bin/chromedriver", options=chrome_options)
+
 
 from bs4 import BeautifulSoup
 import time
