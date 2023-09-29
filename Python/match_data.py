8c0063f027316f487e908eedb314e376130065cf
attkap <130363145+attkap@users.noreply.github.com>
2023-09-29 17:12:37 +0200

    Update match_data.py

--- Python/match_data.py
+++ Python/match_data.py
@@ -17,7 +17,12 @@ chrome_options = Options()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
-driver = webdriver.Chrome(options=chrome_options)
+chrome_options.add_argument("--enable-logging")
+chrome_options.add_argument("--v=1")
+chrome_options.add_argument("--disable-gpu")
+
+driver = webdriver.Chrome(executable_path="/usr/bin/chromedriver", options=chrome_options)
+
 
 from bs4 import BeautifulSoup
 import time
